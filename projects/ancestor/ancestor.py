27dbb7c4566ae2b634fd00226d9b8b973f7afc07
AshtonRagan <arkjr1@gmail.com>
2020-06-10 00:18:47 -0500

    day 2 almost done

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,25 @@
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+
+    l = [2]
+    d = {}
+    q = ancestors
+    for i in q:
+        if i[0] in d:
+            d[i[0]].append(i[1])
+        else:
+            d[i[0]] = [i[1]]
+
+    for i in d:
+        for j in d[i]:
+            # print(l)
+            if j == l[0]:
+                l.append(i)
+
+    print(l)
+
+
+# {1: [3], 2: [3], 3: [6], 5: [6, 7], 4: [5, 8], 8: [9], 11: [8], 10: [1]}
+a = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7),
+     (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
+earliest_ancestor(a, 1)
